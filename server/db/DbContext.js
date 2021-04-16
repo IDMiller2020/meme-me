64fa1616e38f80fcc0a0278f6029faedb37a519f
Toppedyk <Tayloroppedyk@yahoo.com>
2021-04-16 15:36:25 -0600

    fixed db context

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -1,12 +1,10 @@
 import mongoose from 'mongoose'
-import UserSchema from '../models/User'
 import PostSchema from '../models/Post'
 import CommentSchema from '../models/Comment'
 
 import AccountSchema from '../models/Account'
 
 class DbContext {
-  Users = mongoose.model('User', UserSchema);
   Posts = mongoose.model('Post', PostSchema);
   Comments = mongoose.model('Comment', CommentSchema);
   Account = mongoose.model('Account', AccountSchema);
